3276d9bf9435ec81ec8b41a30d77ee50fdd4669e
liyi <liyi@dftcmedia.com>
2017-07-05 13:36:00 +0800

    change

--- app/src/main/java/com/dftc/videoplay/MainActivity.java
+++ app/src/main/java/com/dftc/videoplay/MainActivity.java
@@ -17,10 +17,10 @@ public class MainActivity extends AbsVideoScreenSwitchActivity {
 //    private String url = "rtsp://192.168.2.67:8554/MainStream";       //监控摄像头测试可行
 //    private String url = "http://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f30.mp4";   //测试可行
 //    private String url = "rtsp://192.168.6.77:8554/730446.sdp";       //测试可行
-//    private String url = "file:///storage/emulated/0/Download/apes.mp4";    //本地高清mp4视频，测试可行
+    private String url = "file:///storage/emulated/0/Download/apes.mp4";    //本地高清mp4视频，测试可行
 //    private String url = "file:///storage/emulated/0/Download/rmvb-test.rmvb";    //rmvb不可行
 //    private String url = "file:///storage/emulated/0/Download/avi-test.avi";      //avi不可行
-        private String url = "file:///storage/emulated/0/Download/split.avi";      //avi不可行
+//        private String url = "file:///storage/emulated/0/Download/split.avi";      //avi不可行
 //    private String url = "file:///storage/emulated/0/Download/mkv-test.mkv";        //mkv不可行
 
     @Override
